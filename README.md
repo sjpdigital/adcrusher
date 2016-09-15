7a39f3a510e9bd374b5ff791ff8c4545ff96a93f
Julian Bravard <julian.bravard@gmail.com>
2016-09-15 09:23:05 -0400

    Init with base script and readme.

---
+++ README.md
@@ -0,0 +1,28 @@
+# Ad Crusher
+
+This script is for crushin' ads. Feed it one or more folders containing assets
+related to display ads (banners), and it'll do its best to get that size down
+to a minimum. It will output a .zip in the same directory that should be ready
+to upload to an ad server (like Flashtalking).
+
+## Dependencies
+This script requires you have a few things available in your path:
+- `pngquant`, available via `brew`
+- `jpegoptim`, also available via `brew`
+- `uglifyjs`, installed globally via `npm` (`npm install --global uglify-js`)
+- `html-minifier`, also an `npm` package (`npm install --global html-minifier`)
+
+It should complain if you're missing these.
+
+## Bugs
+If this script makes things broken, open up an issue and I'll take a look!
+
+## Roadmap
+I'm considering rewriting this as a node module, to allow for more
+configuration and an easier installation process. Other possible additions
+include the ability to automatically load ad server components, and verifying
+or automatically generating the `manifest.js` file required by Flashtalking. Or
+anything else, really, the possibilities are endless!
+
+One thing that could be implemented quickly would be switch options for
+controlling the desired JPEG optimization level (currently set to 70).
---
+++ adcrusher.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+command -v pngquant >/dev/null 2>&1 || { printf >&2 "I require pngquant but it's not installed. Aborting."; exit 1; }
+command -v jpegoptim >/dev/null 2>&1 || { printf >&2 "I require jpegoptim but it's not installed. Aborting."; exit 1; }
+command -v uglifyjs >/dev/null 2>&1 || { printf >&2 "I require uglifyjs but it's not installed. Aborting."; exit 1; }
+command -v html-minifier >/dev/null 2>&1 || { printf >&2 "I require html-minifier but it's not installed. Aborting."; exit 1; }
+
+pushd () {
+  command pushd "$@" > /dev/null
+}
+
+popd () {
+  command popd "$@" > /dev/null
+}
+
+if (( $# < 1 ))
+then
+  printf "Usage: adcrusher folder [ folder ]"
+  exit 1
+fi
+
+for var in "$@"
+do
+  if [ ! -d "$var" ]; then
+    printf "$var is not a directory, skipping..."
+    continue
+  else
+    dest="crushed-$var"
+    rm -rf "$dest"
+    cp -rf "$var" "$dest"
+
+    printf "entering $var"
+    pushd "$dest"
+
+    printf "minify .png\n"
+    find . -name "*.png" -exec pngquant --skip-if-larger --force --ext .png --speed 1 {} \;
+
+    printf "minify .jpg\n"
+    find . -name "*.jpg" -exec jpegoptim -m 70 -t -s {} \;
+
+    printf "minify .js\n"
+    find . -name "*.js" ! -name "manifest.js" -exec uglifyjs --compress --mangle --screw-ie8 --output {} -- {} \;
+
+    printf "minify .html\n"
+    find . -name "*.html" \
+      -exec html-minifier \
+      --case-sensitive \
+      --collapse-boolean-attributes \
+      --collapse-inline-tag-whitespace \
+      --collapse-inline-tag-whitespace \
+      --collapse-whitespace \
+      --html5 \
+      --minify-cs \
+      --minify-js \
+      --remove-comments \
+      --remove-empty-attributes \
+      --output "{}.min" {} \; \
+      -exec mv "{}.min" {} \;
+
+    printf "zipping...\n"
+    zip -r "../$var.zip" *
+
+    printf "leaving $var\n"
+    popd
+
+    printf "cleanup...\n"
+    rm -rf "$dest"
+
+    printf "\ndone!\n"
+  fi
+done
